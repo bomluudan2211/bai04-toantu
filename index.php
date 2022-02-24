16a17f6e1331e76e684bc191644b0d2ac3d169d4
bomluudan2211 <97217594+bomluudan2211@users.noreply.github.com>
2022-02-24 16:32:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    $a = 5;
+    $b = 3;
+    $tong = $a + $b;
+    echo "Tong: $tong <br>";
+
+    $hieu = $a - $b;
+    echo "Hieu: $hieu <br>";
+
+    $tich = $a * $b;
+    echo "Tich: $tich <br>";
+
+    $thuong = $a / $b;
+    echo "Thuong: $thuong <br>";
+
+    $sodu = $a % $b;
+    echo "Sodu: $sodu <br>"; 
+
+    $ss_bang = $a == $b;
+    echo "So sanh: $ss_bang <br>";
+
+
+    $ss_khac = $a != $b;
+    echo "So sanh khac: $ss_khac <br>";
+
+    $ss_lon_hon = $a > $b;
+    echo "So sanh lon hon: $ss_lon_hon <br>"; 
+?>
+
+
+  
+
+
+
+
